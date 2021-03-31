9cf3e4b2a58bd40f939c7d36e3e7836cf06fe94c
vishalpwaman <63911951+vishalpwaman@users.noreply.github.com>
2021-03-31 19:25:38 +0530

    Added: Mysql Database setting

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+# class product(models.model):
+#     name = models.CharField(max_length=200, null=False, blank=False)
+#     category = models.CharField(max_length=100, null=False, blank=False)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     description = models.TextField()
+#     stars = models.IntegerField()
---
+++ api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from .models import Task
+
+# Create your views here.
+
