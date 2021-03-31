171a491da39ec32996c7219ec3f3042765b0ae89
vishalpwaman <63911951+vishalpwaman@users.noreply.github.com>
2021-03-31 21:29:21 +0530

    Added: Create Database table & Get all Data Api

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views, name=""),
+    # path('', views.apiOverview, name="apiOverview"),
+    path('', views.productalllist, name="productalllist"),
 ]
